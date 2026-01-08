d8e293f3d486152699eacaaa67995fabfd969866
Mr.Muzhi <bbcc602@163.com>
2026-01-08 18:30:52 +0800

    feat(All): 添加墨云阁组件，以及插件组件模板
    添加墨云阁组件，以及插件组件模板

---
+++ plugins/system/reload-plugins.ts
@@ -0,0 +1,18 @@
+import type {MessageContext} from "@mtcute/dispatcher";
+import {BasePlugin, PluginScope} from "../../src/core/base-plugin.js";
+
+export default class ReloadPlugins extends BasePlugin {
+    command = 'pg';
+    name = '插件';
+    description = '用于插件操作';
+    scope = 'both' as PluginScope;
+
+    async handlerCommand(message: MessageContext, subCommand: string, args: string[]): Promise<void> {
+
+    }
+
+    async handleMessage(message: MessageContext): Promise<void> {
+        console.log('收到消息了', message);
+    }
+
+}
